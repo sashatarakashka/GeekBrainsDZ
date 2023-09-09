08d46fc39ddf9e1a3ac18d7b682a2dc956064965
sashkatarakashka <lapakoza@gmail.com>
2023-09-10 00:41:19 +0300

    first commit

---
+++ 2zadacha/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите два числа ");
+string text = Console.ReadLine();
+int a = Convert.ToInt32(text);
+string _text2 = Console.ReadLine();
+int b = Convert.ToInt32(_text2);
+if (a>b)
+{
+    Console.WriteLine(a);
+    Console.Write("Наибольшее число");
+}
+if (b>a)
+{
+    Console.WriteLine(b);
+    Console.Write("Наибольшее число");
+}
+  Console.ReadKey();
