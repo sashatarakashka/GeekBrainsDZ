c3767f167e6a7447705532b252f820b3015478e3
sashkatarakashka <lapakoza@gmail.com>
2023-09-10 00:49:13 +0300

    Решила задачу 4

---
+++ 4zadacha/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите три числа ");
+string text = Console.ReadLine();
+int a = Convert.ToInt32(text);
+string _text2 = Console.ReadLine();
+int b = Convert.ToInt32(_text2);
+string _text3 = Console.ReadLine();
+int c = Convert.ToInt32(_text3);
+int max = a;
+
+if (a > max) max = a ;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Большее число=");
+Console.WriteLine(max);
+  
+  Console.ReadKey();
