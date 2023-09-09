477a13c4f21da4fc4958b34a05d657e7bc6372f3
sashkatarakashka <lapakoza@gmail.com>
2023-09-10 01:58:10 +0300

    выполнена задача 6

---
+++ 8zadacha/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+             string text= Console.ReadLine();
+            int n = Convert.ToInt32(text);
+            int i=0;
+             for(i=0;i <= n;i++)
+                {
+                  
+             Console.Write("{0}", i++);
+         }
+            Console.ReadKey();
