633fcef11111a6d9a603056bf96bf1b7ee931d91
sashkatarakashka <lapakoza@gmail.com>
2023-09-10 00:54:19 +0300

    Решили задачу 6

---
+++ 6zadacha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+string text = Console.ReadLine();
+int x = Convert.ToInt32(text);
+if(x%2==0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+Console.ReadKey();
